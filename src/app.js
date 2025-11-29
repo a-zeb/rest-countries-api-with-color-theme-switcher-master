f422d9835421d1ee75a33cee3d48e15d2a2b7fb4
azebusmc <65170242+azebusmc@users.noreply.github.com>
2025-11-29 00:49:39 -0500

    Added layout, HTML structure, and toggle boilerplate

--- src/app.js
+++ src/app.js
@@ -1,3 +1,13 @@
-function start() {}
+const themeButton = document.getElementById('theme-toggle');
+
+function start() {
+  if (themeButton) {
+    themeButton.addEventListener('click', toggleTheme);
+  }
+}
+
+function toggleTheme() {
+  document.body.classList.toggle('dark-mode');
+}
 
 start();
